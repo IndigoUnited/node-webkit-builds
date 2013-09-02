b1ed06d40f3881cbbd0688aa6d5198d2c4a2871d
Marco Oliveira <me@marcooliveira.com>
2013-09-03 00:07:23 +0100

    wip

---
+++ tasks/update-module.autofile.js
@@ -0,0 +1,97 @@
+/*jshint es5:true*/
+
+'use strict';
+
+var download = require('autofile-download');
+var mkdir    = require('autofile-mkdir');
+var rm       = require('autofile-rm');
+var fs       = require('fs');
+var semver   = require('semver');
+
+var pkgJsonFile = __dirname + '/../package.json';
+
+module.exports = function (task) {
+    task
+    .id('update-module')
+    .name('update-module')
+    .author('Indigo United')
+
+    .setup(function (opts, ctx, next) {
+        opts.readMeFile = __dirname + '/tmp/node-webkit-readme.md';
+
+        next();
+    })
+
+    // .do(rm, {
+    //     description: 'Assert tmp dir is clear',
+    //     options: {
+    //         files: __dirname + '/tmp'
+    //     }
+    // })
+
+    // .do(mkdir, {
+    //     options: {
+    //         dirs: __dirname + '/tmp'
+    //     }
+    // })
+
+    // .do(download, {
+    //     description: 'Download node-webkit README.md file',
+    //     options: {
+    //         files: {
+    //             'https://raw.github.com/rogerwang/node-webkit/master/README.md': '{{readMeFile}}'
+    //         }
+    //     }
+    // })
+
+    .do(function (opts, ctx, next) {
+        fs.readFile(opts.readMeFile, {
+            encoding: 'utf8'
+        }, function (err, data) {
+            if (err) {
+                return next(err);
+            }
+            // Links should follow the following pattern:
+            // * Linux: [32bit](URL) / [64bit] (URL)
+            // * Windows: [win32](URL)
+            // * Mac: [32bit, 10.7+](URL)
+
+            // capture the links
+            var linux32 = /linux.*32bit[^\(]*\((http[^\)]+)\)/i.exec(data)[1],
+                linux64 = /linux.*64bit[^\(]*\((http[^\)]+)\)/i.exec(data)[1],
+                win32   = /win32[^\(]*\((http[^\)]+)\)/i.exec(data)[1],
+                mac32   = /mac.*32bit[^\(]*\((http[^\)]+)\)/i.exec(data)[1],
+                tmp     = /\[v([^\s]+)\s+release notes\]\(([^\)]+)/i.exec(data),
+                version = tmp[1],
+                releaseNotes = tmp[2];
+
+            ctx.log.debugln('Captured version:', version);
+            ctx.log.debugln('linux64:', linux64);
+            ctx.log.debugln('linux32:', linux32);
+            ctx.log.debugln('win32:', win32);
+            ctx.log.debugln('mac32:', mac32);
+            ctx.log.debugln('release notes:', releaseNotes);
+            
+
+            var pkg = require(pkgJsonFile);
+
+            if (!semver.valid(version)) {
+                next(new Error('Unexpected version in README.md:', version));
+            }
+
+            // if the node-webkit release is greater than the module version
+            if (semver.gt(version, pkg.version)) {
+                ctx.log.infoln('Found new version:', pkg.version, '->', version);
+                pkg.version = version;
+
+                fs.writeFile(pkgJsonFile, JSON.stringify(pkg, null, '  '), function (err) {
+                    next(err);
+                });
+            }
+
+            next();
+        });
+    }, {
+        description: ''
+    });
+};
